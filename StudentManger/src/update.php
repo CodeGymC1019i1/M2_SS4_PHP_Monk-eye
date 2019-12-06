2e56b452e948e117c2ebfef1fe0cfd80b66e65a5
buihoang <hoangbuivan0962654936@gmail.com>
2019-12-06 09:44:22 +0700

    sutudent

---
+++ StudentManger/src/update.php
@@ -0,0 +1,17 @@
+<?php
+$index = (int)$_GET['index'];
+//var_dump($index);
+//die();
+$listStudent = file_get_contents('../data.json');
+$listStudent = json_decode($listStudent,true);
+
+$listStudent[$index] = [
+  "name" => $_GET["name"],
+  "age" => $_GET["age"],
+  "address" => $_GET["address"],
+  "group" => $_GET["group"]
+];
+$newListStudent = json_encode($listStudent);
+file_put_contents('../data.json',$newListStudent);
+header("Location:../index.php");
+?>
